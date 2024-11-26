01ace09ff7a87232e73971a7ed91e0a0de1e61a4
Martin Sevov <ms.sevov@gmail.com>
2024-11-26 11:20:28 +0100

    Recreate Project Structure. Write sprintoutcomes.org file.

--- afmers/api/apps.py
+++ afmers/api/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class BackEndConfig(AppConfig):
+class ApiConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "back_end"
+    name = "api"

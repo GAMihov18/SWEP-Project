dd49ce41fac730ce5943f81a82271181327d5e3f
Martin Sevov <ms.sevov@gmail.com>
2024-11-27 11:20:41 +0100

    Add __str__ to models

--- afmers/api/models.py
+++ afmers/api/models.py
@@ -81,6 +81,9 @@ class Report(BaseModel):
     )
     has_task = models.BooleanField(default=False)
 
+    def __str__(self):
+        return self.title
+
 
 class Task(BaseModel):
     """
@@ -97,3 +100,6 @@ class Task(BaseModel):
     status = models.CharField(
         max_length=1, choices=STATUS_OPTIONS, default="N", blank=False
     )
+
+    def __str__(self):
+        return self.title

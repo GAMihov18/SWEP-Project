048b634ede45858ff7d24cd9f4205c23e19b4155
Martin Sevov <ms.sevov@gmail.com>
2024-11-27 19:59:38 +0100

    Extract functionality of Login to front_end.views

--- afmers/afmers/urls.py
+++ afmers/afmers/urls.py
@@ -18,7 +18,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from front_end import views
 
 import front_end.views as fe
 import api.views as api
@@ -27,7 +26,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", api.root),
     path("api/reports", api.reports),
-    path("", views.Home, name="home-page"),
-    path("login/", LoginView.as_view(template_name="Login.html"), name="login-page"),
-    path("register/", views.register, name="register-page"),
+    path("", fe.home, name="home-page"),
+    path("login/", fe.login, name="login-page"),
+    path("register/", fe.register, name="register-page"),
 ]
--- afmers/front_end/views.py
+++ afmers/front_end/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView, LogoutView
 from front_end.forms import AccountCreationForm
 
 # Create your views here.
 
 
-def Home(request):
+def home(request):
     return render(request, "base.html")
 
 
@@ -18,3 +19,6 @@ def register(request):
     else:
         form = AccountCreationForm()
     return render(request, "Register.html", {"form": form})
+
+
+login = LoginView.as_view(template_name="Login.html")
